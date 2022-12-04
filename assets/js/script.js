4580e1adeca690c2cd187dcd203ed67a9263f56b
Jeffrey Tran <108315442+Jtran028@users.noreply.github.com>
2022-12-04 04:40:01 -0800

    working search function populates main city weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,14 @@
-const container = document.querySelector(".container"),
-inputCity = container.querySelector(".input-city"),
-inputCityName = inputCity.querySelector(".city-name"),
-inputSearch = inputCity.querySelector("input");
+var container = document.querySelector("container");
+var inputCity = document.querySelector("input-city");
+var inputCityName = document.querySelector("city-name");
+var inputSearch = document.querySelector("input");
+var cityMain = document.querySelector("#city-main");
+var fiveDay = document.querySelector("#five-day")
+var weatherIcon = document.querySelector("#weather-icon")
 
+let api;
+
+let forecast;
 // event listener for search bar to request API data when ENTER key pressed
 
 inputSearch.addEventListener ("keyup", e => {
@@ -16,13 +22,55 @@ var apiKey = "13f00cd7501485b4015676dd279bd9fe"
 //function to fetch api data from openweather.api and turns ojbect into JSON
 
 function requestApi(city) {
-    let api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
-    fetch(api).then(response => response.json()).then(result => cityWeather(result));
+    api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+    forecast = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=imperial`;     
+    fetchData();
+    forcastData();
     
 }
 
-function cityWeather(info){
-    console.log(info);
+function fetchData() {
+    fetch(api).then(response => response.json()).then(result => cityWeather(result));
+
     
 }
+
+function forcastData() {
+    fetch(forecast).then(response => response.json()).then(result => cityForecast(result))
+    
+}
+
+function cityForecast(data) {
+    
+
+    console.log(data);
+}
+
+function cityWeather(info){
+    //removes hidden cards
+    cityMain.removeAttribute("hidden");
+    fiveDay.removeAttribute("hidden");
+    console.log(info);
+
+    let currentDate = moment(info.dt.value).format("MMM D YYYY");
+
+    // variables for ojbect data values
+
+    var cityName = info.name;
+    var cityDegree = info.main.temp;
+    var cityHumidity = info.main.humidity;
+    var cityWind = info.wind.speed;
+
+    //replaces current elements with fetched info
+
+    document.querySelector(".display-2").innerText = cityName;
+    document.querySelector(".current-date").innerText = currentDate;
+    document.querySelector(".city-degree").innerText ="Temperature:" + cityDegree + "°F";
+    document.querySelector(".city-humidity").innerText = "Humidity: " + cityHumidity + " %";
+    document.querySelector(".city-windspeed").innerText = "Wind: " + cityWind + " mph"
+    // document.querySelector(".city-uv").innerText = cityName;
+    weatherIcon.setAttribute("src", `https://openweathermap.org/img/wn/${info.weather[0].icon}@2x.png`);
+    
+}
+
 
